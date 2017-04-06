103e0ba9553cb499087cc3b8a3eafc0b218171bd
Nick Krumholz <nick.krumholz@gmail.com>
2017-04-06 17:29:34 -0500

    Moved analytics to a new file

--- elements/header_top.php
+++ elements/header_top.php
@@ -12,13 +12,13 @@
 		'pageMetaKeywords' => isset($pageMetaKeywords) ? $pageMetaKeywords : ''
 	]);?>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-		
+
 <!-- Start of Async Drift Code -->
 <script>
 !function() {
 	var t;
-	if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0, 
-	t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+	if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
+	t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
 	t.factory = function(e) {
 		return function() {
 			var n;
@@ -28,8 +28,8 @@
 		t[e] = t.factory(e);
 	}), t.load = function(t) {
 		var e, n, o, i;
-		e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"), 
-		o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js", 
+		e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"),
+		o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js",
 		n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
 	});
 }();
@@ -37,8 +37,11 @@ drift.SNIPPET_VERSION = '0.3.1';
 drift.load('z4dvzcnki75t');
 </script>
 <!-- End of Async Drift Code -->
-		
+
 </head>
 
 <body>
+
+<?php $this->inc('elements/analyticstracking.php');?>
+
 <div class="<?php echo $c->getPageWrapperClass()?>">
