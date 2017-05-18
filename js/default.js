e84685e28515f7dd442a9b5b3afc0cde78a71b16
Natasha Gaffer <ngaffer@gmail.com>
2017-05-18 13:15:14 -0500

    updated js so scrolled class appears only once on scroll

--- js/default.js
+++ js/default.js
@@ -2,7 +2,7 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header-bar").className += " scrolled";
+        document.getElementById("header-bar").className = "scrolled";
     } else {
         document.getElementById("header-bar").classList.remove("scrolled");
     }
